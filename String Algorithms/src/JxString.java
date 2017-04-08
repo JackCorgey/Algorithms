dbdc089ea0ae5b892a022388e9d66120c9a6ff01
JackCorgey <jcorgey@outlook.com>
2017-04-08 14:01:36 -0500

    Implemented Boyer Moore

--- String Algorithms/src/JxString.java
+++ String Algorithms/src/JxString.java
@@ -1,38 +1,67 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class JxString {
-
-	public static ArrayList<Integer> simple_search(String _needle, String _haystack) {
-		char[] needle = _needle.toCharArray(),
-			   haystack = _haystack.toCharArray();	
+	
+	static class Boore_Moore {
 		
-		int in = 0;
-		ArrayList<Integer> matched = new ArrayList<Integer>();
-		while( (needle.length+in) <= haystack.length ) {
-			int jmp = isMatch(needle, haystack, in);
-			if(jmp == -1)
-				matched.add(in++);
-			else 
-				in += jmp;
+		String needle, haystack;
+		HashMap<Character,Integer> jmpIndex;
+		
+		Boore_Moore(String needle, String haystack) {
+			this.needle = needle;
+			this.haystack = haystack;
+			iniJmpIndex();
 		}
 		
-		return matched;
+		void iniJmpIndex() {
+			jmpIndex = new HashMap<Character,Integer>();
+			for(int i = 0, len = needle.length()-1; i < len; ++i)
+				jmpIndex.put(needle.charAt(i), needle.length()-1 - i);
+		}
+		
+		ArrayList<Integer> boyer_moore_search() {	
+			int in = 0;
+			ArrayList<Integer> matched = new ArrayList<Integer>();
+			while( (needle.length()+in) <= haystack.length() ) {
+				int jmp = getJmp(in);
+				System.out.println(jmp);
+				if(jmp == -1)
+					matched.add(in++);
+				else 
+					in += jmp;
+			}
+			
+			return matched;
+		}
+		
+		int getJmp(int in) {
+			int match = needle.length()-1;
+			while(match >= 0) {
+				if(needle.charAt(match) != haystack.charAt(in+match))
+					break;
+				--match;
+			}
+			
+			if(match == -1)
+				return match;
+			
+			char anchor = haystack.charAt(in + needle.length()-1);
+			return ( jmpIndex.containsKey(anchor) ) ? jmpIndex.get(anchor) : needle.length();
+			
+		}
+		
+		String test() {
+			return "test";
+		}
+		
 	}
 	
-	private static int isMatch(char[] needle, char[] haystack, int in) {
-		int match = needle.length-1;
-		while(match >= 0) {
-			if(needle[match] == haystack[in+match])
-				--match;
-			else 
-				break;
-		}
-		
-		if(match != -1)
-			match = (needle.length - match);
-		
-		return match;
+	public static ArrayList<Integer> findAll(String needle, String haystack) {
+		Boore_Moore bm = new Boore_Moore(needle, haystack);
+		return bm.boyer_moore_search();
 	}
+
 	
 }
--- String Algorithms/src/test.java
+++ String Algorithms/src/test.java
@@ -5,12 +5,13 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String needle = "aaaaaaaaaaaaaaaaaa";
-		//String haystack = "patternlatternpatternpatternlatternpatternlattern";
-		String haystack = "aaaaaaaaaa";
+		String needle = "a";
+		String haystack = "aaaaaa";
 		
-		ArrayList<Integer> matched = JxString.simple_search(needle, haystack);
-		System.out.println(matched);
+		System.out.println( JxString.findAll(needle, haystack) );
+		
+		//ArrayList<Integer> matched = JxString.simple_search(needle, haystack);
+		//System.out.println(matched);
 		
 	}
 	
